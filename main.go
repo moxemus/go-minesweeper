d080b6ddc60b0498fa05defe0e4498b6ca14f2b2
Maksim Partsiankou <maksim.part@gmail.com>
2026-01-13 10:46:20 +0100

    add input handler

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math/rand"
 	"strconv"
@@ -10,82 +11,85 @@ const height = 9
 const width = 9
 const bombs_count = 5
 
+type Point struct {
+	X int
+	Y int
+}
+
 type Cell struct {
 	Value   int
 	Visible bool
 }
 
 type GameMap struct {
-	Height    int
-	Width     int
-	BombCount int
-	Grid      [][]Cell
+	height    int
+	width     int
+	bombCount int
+	grid      [][]Cell
 }
 
 func (g *GameMap) Init(h, w, bombs int) {
-	g.Height = h
-	g.Width = w
-	g.BombCount = bombs
-	g.Grid = make([][]Cell, g.Height)
+	g.height = h
+	g.width = w
+	g.bombCount = bombs
+	g.grid = make([][]Cell, g.height)
 
 	// Init map
-	for i := 0; i < g.Height; i++ {
-		g.Grid[i] = make([]Cell, g.Width)
+	for i := 0; i < g.height; i++ {
+		g.grid[i] = make([]Cell, g.width)
 
-		for j := 0; j < g.Width; j++ {
-			g.Grid[i][j].Value = 0
-			g.Grid[i][j].Visible = false
+		for j := 0; j < g.width; j++ {
+			g.grid[i][j].Value = 0
+			g.grid[i][j].Visible = false
 		}
 	}
 
 	// Add bombs
-	for i := 0; i < g.BombCount; i++ {
-		y := rand.Intn(g.Height)
-		x := rand.Intn(g.Width)
+	for i := 0; i < g.bombCount; i++ {
+		y := rand.Intn(g.height)
+		x := rand.Intn(g.width)
 
-		g.Grid[x][y].Value = -1
+		g.grid[x][y].Value = -1
 
 		for dy := -1; dy <= 1; dy++ {
 			for dx := -1; dx <= 1; dx++ {
 				ny, nx := y+dy, x+dx
 
-				if ny < 0 || ny >= g.Height || nx < 0 || nx >= g.Width || g.Grid[nx][ny].Value == -1 {
-					continue
+				if ny >= 0 && ny < g.height && nx >= 0 && nx < g.width && g.grid[nx][ny].Value != -1 {
+					g.grid[nx][ny].Value++
 				}
-
-				g.Grid[nx][ny].Value++
 			}
 		}
 	}
 }
 
-func (g *GameMap) openZeros(x, y int) {
+func (g *GameMap) openZeros(point Point) {
 	for dx := -1; dx <= 1; dx++ {
 		for dy := -1; dy <= 1; dy++ {
-			nx := x + dx
-			ny := y + dy
+			nx := point.X + dx
+			ny := point.Y + dy
 
-			if nx < 0 || ny < 0 || nx >= width || ny >= g.Height {
+			if nx < 0 || ny < 0 || nx >= width || ny >= g.height {
 				continue
 			}
 
-			if g.Grid[nx][ny].Visible {
+			if g.grid[nx][ny].Visible {
 				continue
 			}
 
-			g.Grid[nx][ny].Visible = true
+			g.grid[nx][ny].Visible = true
 
-			if g.Grid[nx][ny].Value == 0 {
-				g.openZeros(nx, ny)
+			if g.grid[nx][ny].Value == 0 {
+				g.openZeros(Point{X: nx, Y: ny})
 			}
 		}
 	}
 }
 
 func (g *GameMap) checkWin() bool {
-	for i := 0; i < g.Height; i++ {
-		for j := 0; j < g.Width; j++ {
-			if g.Grid[j][i].Value != -1 && !g.Grid[j][i].Visible {
+	for i := 0; i < g.height; i++ {
+		for j := 0; j < g.width; j++ {
+			if g.grid[j][i].Value != -1 && !g.grid[j][i].Visible {
 				return false
 			}
 		}
@@ -94,15 +98,17 @@ func (g *GameMap) checkWin() bool {
 	return true
 }
 
-func (g *GameMap) pressCell(x, y int) bool {
-	g.Grid[x][y].Visible = true
+func (g *GameMap) pressCell(point Point) bool {
+	g.grid[point.X][point.Y].Visible = true
 
-	return g.Grid[x][y].Value == -1
+	return g.grid[point.X][point.Y].Value == -1
 }
 
-// Screen Handler
+// Drawer logic
 type Drawer interface {
-	drawMap()
+	drawMap(cells [][]Cell, height, width int)
+	write(s string)
+	clearScreen()
 }
 
 type TerminalDrawer struct {
@@ -116,7 +122,7 @@ func Init(d Drawer) *TerminalDrawer {
 }
 
 func (s *TerminalDrawer) write(text string) {
-	fmt.Print(text)
+	fmt.Println(text)
 }
 
 func (s *TerminalDrawer) drawMap(cells [][]Cell, height, width int) {
@@ -150,49 +156,80 @@ func (s *TerminalDrawer) drawMap(cells [][]Cell, height, width int) {
 	}
 }
 
-func (s *TerminalDrawer) clear() {
+func (s *TerminalDrawer) clearScreen() {
 	fmt.Println("\033[H\033[2J")
+}
+
+// User input logic
+type UserMessage struct {
+	Point
+	Command string
+}
+
+type UserInput interface {
+	getInput() (UserMessage, error)
+}
+
+type TerminalInput struct {
+	input UserInput
+}
+
+func (t *TerminalInput) getInput() (UserMessage, error) {
+	var input string
+	_, err := fmt.Scanln(&input)
+	userMessage := UserMessage{}
+
+	if len(input) == 3 && input[0] >= '1' && input[0] <= '9' && input[2] >= '1' && input[2] <= '9' && err == nil {
+		userMessage.X = int(input[0]-'0') - 1
+		userMessage.Y = int(input[2]-'0') - 1
+	} else if len(input) == 1 && err == nil {
+		userMessage.Command = input
+	} else {
+		return userMessage, errors.New("Invalid input. Please select cell coordinates in format X,Y")
+	}
+
+	return userMessage, nil
 }
 
 func main() {
 	gameMap := &GameMap{}
 	gameMap.Init(height, width, bombs_count)
 
-	drawHandler := &TerminalDrawer{}
+	var drawHandler Drawer
+	var inputHandler UserInput
 
-	var input string
+	drawHandler = &TerminalDrawer{}
+	inputHandler = &TerminalInput{}
 
 	for {
-		drawHandler.drawMap(gameMap.Grid, gameMap.Height, gameMap.Width)
+		drawHandler.drawMap(gameMap.grid, gameMap.height, gameMap.width)
 
-		_, err := fmt.Scanln(&input)
-		drawHandler.clear()
+		userMessage, err := inputHandler.getInput()
+
+		drawHandler.clearScreen()
 
 		// Handle user input
-		if len(input) == 3 && input[0] >= '1' && input[0] <= '9' && input[2] >= '1' && input[2] <= '9' && err == nil {
-			x := int(input[0]-'0') - 1
-			y := int(input[2]-'0') - 1
-
-			// Handle cell click and check is user was exploded
-			if gameMap.pressCell(x, y) {
+		if err != nil {
+			drawHandler.write("Invalid input")
+		} else if userMessage.Command == "" {
+			if gameMap.pressCell(userMessage.Point) {
 				drawHandler.write("Game over. Game restarted.")
 				gameMap.Init(height, width, bombs_count)
 			} else {
-				drawHandler.write("Your choise is: " + strconv.Itoa(x+1) + strconv.Itoa(y+1) + " result - 0")
-				gameMap.openZeros(x, y)
+				drawHandler.write("Your choise is: " + strconv.Itoa(userMessage.X+1) + " " + strconv.Itoa(userMessage.Y+1))
+				gameMap.openZeros(userMessage.Point)
 			}
 
 			if gameMap.checkWin() {
 				drawHandler.write("Victory! Game restarted.")
 				gameMap.Init(height, width, bombs_count)
 			}
-
-		} else if input == "q" {
+		} else if userMessage.Command == "q" {
 			break
-		} else if input == "r" {
+		} else if userMessage.Command == "r" {
 			gameMap.Init(height, width, bombs_count)
 		} else {
-			drawHandler.write("Invalid input. Please select cell coordinates in format X,Y")
+			drawHandler.write("Invalid command")
 		}
 	}
 }
